cc0617f9fafe05a60fe3fe87a78b1e6e4862cae1
ahmad-ben <ahmedbenchakhter@gmail.com>
2025-02-19 15:40:45 +0100

    Add Project Demo Link.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,6 @@ import { NgxSpinnerModule } from 'ngx-spinner';
 import { ToastrService } from 'ngx-toastr';
 import { WebRequestService } from './services/web/web-request.service';
 import { MenusStateService } from './services/subjects/menus-state.service';
-import { merge, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -20,6 +19,17 @@ import { merge, Subject } from 'rxjs';
 })
 export class AppComponent implements OnInit {
   title = 'NoteItNow';
+  demoLink = `
+    <a 
+      href="https://youtu.be/U6L4dNBETH0?si=Y9gNLkmNbNx0ovQE" target="_blank"
+    >See Website Demo</a>
+  `;
+  message = `
+    The back-end is hosted on free providers.<br>
+    You may experience a delay of up to 1 minute on your first request.<br>
+    Thank you for your understanding 🌹.<br>
+    ${this.demoLink}
+  `;
 
   toastr = inject(ToastrService);
   webReqService = inject(WebRequestService);
@@ -30,13 +40,12 @@ export class AppComponent implements OnInit {
     
     if(!notNewVisiter){
       sessionStorage.setItem("notNewVisiter", "yes");
-      this.toastr.warning(
-        `The back-end is hosted on free providers. 
-        You may experience a delay on your first request. 
-        Thank you for your understanding 🌹.`,
-        "Reminder",
-        { timeOut: 15000, closeButton: true }
-      );      
+      this.toastr.warning(this.message, "Reminder", {
+        timeOut: 30000,
+        closeButton: true,
+        enableHtml: true,
+        toastClass: "ngx-toastr class-test"
+      });    
     };
 
     return this.webReqService.get('test').subscribe();
